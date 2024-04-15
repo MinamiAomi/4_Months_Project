7ef584dbc5a82c9d8cffd257b91ec7d9754387b6
TonosakiRen <ren221603@gmail.com>
2024-04-15 17:53:16 +0900

    ブロックはやした

--- Test/StageBlock.cpp
+++ Test/StageBlock.cpp
@@ -18,10 +18,10 @@ void StageBlock::Initialize(const Vector3& pos, const Vector3& direction) {
 	model_ = std::make_unique<ModelInstance>();
 	model_->SetModel(ResourceManager::GetInstance()->FindModel("box"));
 	model_->SetIsActive(true);
-	transform.scale = { 2.0f,2.0f,36.0f };
-	direction_ = Quaternion::MakeFromEulerAngle(direction);
+	transform.scale = { 2.0f,36.0f,2.0f };
+	direction_ = direction;
 	transform.translate = pos;
-	transform.rotate = Quaternion::MakeFromTwoVector({0.0f,0.0f,1.0f},direction);
+	transform.rotate = Quaternion::MakeFromTwoVector({0.0f,1.0f,0.0f},direction);
 	appearFrame_ = kAppearFrame;
 	isDead_ = false;
 	initialState_ = characterState_;
@@ -30,11 +30,12 @@ void StageBlock::Initialize(const Vector3& pos, const Vector3& direction) {
 void StageBlock::Update() {
 	appearFrame_--;
 	if (appearFrame_ >= 0) {
-		transform.translate.y += 0.4f;
+	//	transform.rotate = Quaternion::MakeForYAxis( 6.0f * Math::ToRadian) * transform.rotate;
+		transform.translate += transform.rotate * Vector3{0.0f,1.0f,0.0f} * 0.4f ;
 	}
 	if (boss_) {
 		float distance = (boss_->transform.worldMatrix.GetTranslate() - transform.translate).Length();
-		if (distance >= 20.0f) {
+		if (distance >= 150.0f) {
 			isDead_ = true;
 		}
 	}
