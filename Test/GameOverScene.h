af75326e5d9e56e8e21c2eef034480fdb2c6bbc5
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2024-06-04 00:55:02 +0900

    ゲームオバー入れてみた

--- Test/GameOverScene.h
+++ Test/GameOverScene.h
@@ -4,9 +4,11 @@
 #include <memory>
 #include <vector>
 
+#include "DebugCamera.h"
 #include "Graphics/LightManager.h"
 #include "Math/Camera.h"
 #include "Graphics/Sprite.h"
+#include "Boss/GameOverBoss/GameOverBoss.h"
 
 class GameOverScene :
 	public BaseScene {
@@ -16,8 +18,11 @@ public:
 	void OnUpdate() override;
 	void OnFinalize() override;
 private:
+	std::unique_ptr<DebugCamera> debugCamera_;
 	std::shared_ptr<Camera> camera_;
 	std::shared_ptr<DirectionalLight> directionalLight_;
 
 	std::unique_ptr<Sprite> title_;
+	std::unique_ptr<GameOverBoss> gameOverBoss_;
+	bool isDebugCamera_;
 };
