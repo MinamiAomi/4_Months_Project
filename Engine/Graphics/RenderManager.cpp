882a9380160d0aa7f89a25b2a07125ca5763d194
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2024-06-04 13:18:38 +0900

    提出用

--- Engine/Graphics/RenderManager.cpp
+++ Engine/Graphics/RenderManager.cpp
@@ -146,9 +146,7 @@ void RenderManager::Render() {
 #ifdef ENABLE_IMGUI
         if (enableWireFrameModel) {
 #endif // ENABLE_IMGUI
-#ifdef _DEBUG
         wireTranslucentRenderer_.Render(commandContext_, *camera, geometryRenderingPass_.GetNormal(), lightingRenderingPass_.GetResult());
-#endif // _DEBUG
 #ifdef ENABLE_IMGUI
         }
 #endif // ENABLE_IMGUI
